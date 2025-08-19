2bb02f9bfc606dc7c3e531fc035d61221d8bda79
masoodn-8 <masoodnawazdune@gmail.com>
2025-08-19 13:28:15 +0530

    Variables in Java

---
+++ variables/Variables.java
@@ -0,0 +1,19 @@
+
+
+public class Variables {
+    public static void main(String[] args) {
+        //This prints in the same line..
+        System.out.print("Sameline");
+        System.out.print("same line");
+
+        //This prints in the next line..
+        System.out.println("This is ");
+        System.out.println("Next line..");
+
+
+        //Variables are useful to store the values..
+
+        int num = 10;
+        System.out.print("The value of variable num is : " + num);
+    }
+}
