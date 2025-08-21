9cc4892dd0bebc105d1b416d8a7379a0e22041ad
masoodn-8 <masoodnawazdune@gmail.com>
2025-08-21 12:12:55 +0530

    Different types of operators in java

---
+++ operators/TerenaryOperator.java
@@ -0,0 +1,17 @@
+
+public class TerenaryOperator {
+    public static void main(String[] args) {
+
+        /*
+         * A shorthand for if-else:
+         * 
+         * condition ? value1 : value2
+         */
+
+        int a = 10, b = 5;
+        int max = (a > b) ? a : b;
+        System.out.println(max); // Output: 10
+
+        
+    }
+}
