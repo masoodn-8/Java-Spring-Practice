c8855bdc35e32a8cbed7e825aea065be640c2a28
masoodn-8 <masoodnawazdune@gmail.com>
2025-08-21 12:08:38 +0530

    Data Types in Java

---
+++ data_types/DataTypes.java
@@ -0,0 +1,26 @@
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+
+        // //Different types of data types..
+        // int num = 8;           //4 bytes
+        // byte b_num = 127;      //1 byte
+        // short s_num = 88;      //2 bytes
+        // long l_num = 888l;     //8 bytes
+
+        // float f_num = 8.0f;    //4 bytes
+        // double d_num = 8.0;    //8 bytes
+
+        // char c = '@';          //2 bytes
+
+        // boolean b = true;      //1 bit (Because true stores '1' & false stores '0')
+
+        // System.out.println("Integer : " + num + " \nByte : " + b_num + " \nShort : " + s_num + " \nLong : " + l_num+ " \nFloat : "+ f_num + " \nDouble : " + d_num + " \nCharacter : " + c + " \nBoolean : " + b);
+        System.out.print("For loop: ");
+        for(int i = 1; i <=5; i++){
+             System.out.print(i + " ");
+       }
+
+    }
+}
